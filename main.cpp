a4797904c7c5de46f14ff128c55f889a443ba8b5
HailMyHeart <llzzmmqwer@outlook.com>
2016-06-08 19:16:50 +0800

    GMU -AI

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a, b, c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cout<<a<<" "<<b<<"  "<<c<<endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
